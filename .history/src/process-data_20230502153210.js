ec9217229dbe8fed17ad8f35c61048122f615c2d
SaraShio <113650568+SaraShio@users.noreply.github.com>
2023-05-02 15:43:41 -0700

    adding difficulty functionality

--- .history/src/process-data_20230502153210.js
+++ .history/src/process-data_20230502153210.js
@@ -26,15 +26,3 @@ export const getQuestions = async function () {
    }
 }
  
-// export const getQuestions = async function (category, difficulty) {
-//    var url = 'https://opentdb.com/api.php?';
-//    if (category) {
-//       url += '&category=' + category;
-//    }
-//    if (difficulty) {
-//       url += '&difficulty=' + difficulty;
-//    }
-//    const data = await fetch(url)
-//       .then(res => res.json());
-//    return data;
-// };
--- .history/src/process-data_20230502154240.js
+++ .history/src/process-data_20230502154240.js
@@ -26,15 +26,15 @@ export const getQuestions = async function () {
    }
 }
  
-// export const getQuestions = async function (category, difficulty) {
-//    var url = 'https://opentdb.com/api.php?';
-//    if (category) {
-//       url += '&category=' + category;
-//    }
-//    if (difficulty) {
-//       url += '&difficulty=' + difficulty;
-//    }
-//    const data = await fetch(url)
-//       .then(res => res.json());
-//    return data;
-// };
+export const getQuestions = async function (category, difficulty) {
+   var url = 'https://opentdb.com/api.php?';
+   if (category) {
+      url += '&category=' + category;
+   }
+   if (difficulty) {
+      url += '&difficulty=' + difficulty;
+   }
+   const data = await fetch(url)
+      .then(res => res.json());
+   return data;
+};
